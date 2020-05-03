002d078d7814373cc873e4c5a1ffc7103f20d92b
cowtaro3 <cowtaro3@gmail.com>
2020-05-03 14:03:08 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const mongodb = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors")
+
+app.use(cors());
+
+const url = 'mongodb+srv://naosan:cowcowcow5@cluster0-yifl2.gcp.mongodb.net/Scraping?retryWrites=true&w=majority';
+const client = new mongodb.MongoClient(url);
+let db, collection;
+client.connect((err) => {
+    if (err) console.log(err);
+    console.log("Connected Successfully");
+    db = client.db("Scraping");
+    collection = db.collection("document");
+})
+
+app.get("/", async (req, res) => {
+    collection.find({}).toArray((err, docs) => {
+        if (err) console.log(err);
+        res.json(docs);
+        console.log(docs);
+    });
+})
+app.listen(8000, console.log("Server started"));
+
+function getTetsugakuNews() {
+    axios.get("http://blog.livedoor.jp/nwknews/")
+        .then(res => {
+            const $ = cheerio.load(res.data);
+            $(".article-title").each((i, el) => {
+                const title = $(el).text();
+                const link = $(el).find("a").attr("href");
+
+                collection.findOne({ "link": link }, (err, doc) => {
+                    if (err) throw err;
+                    if (!doc) {
+                        collection.insert([
+                            { "title": title, "link": link }
+                        ])
+                    }
+                })
+            })
+            console.log("Scraping done...")
+        });
+}
+setInterval(getTetsugakuNews, 1000 * 60 * 60);
